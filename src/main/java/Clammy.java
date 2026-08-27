67f6fc01dc3ef8cbaf96cc2413ef1cd7bb4c9f21
J-unn-y <tanyujun16@gmail.com>
2026-08-27 14:39:01 +0800

    Add storing of inputs into list

--- src/main/java/Clammy.java
+++ src/main/java/Clammy.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
+/**
+ * A simple chatbot that stores tasks entered during the current session.
+ */
 public class Clammy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________\n";
         String startMessage = "Hello! I'm Clammy.\nWhat can I do for you?\n";
         String endMessage = "Bye. Hope to see you again soon!\n";
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println(line + startMessage + line);
         while (scanner.hasNextLine()) {
@@ -14,7 +19,18 @@ public class Clammy {
                 System.out.println(line + endMessage + line);
                 break;
             }
-            System.out.println(line + command + "\n" + line);
+
+            if (command.equals("list")) {
+                System.out.print(line);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.print(line);
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(line + " added: " + command + "\n" + line);
+            }
         }
     }
 }
