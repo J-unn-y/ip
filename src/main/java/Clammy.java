fd51cecc5a0d014424a1156e7eb42bfe81b421d7
J-unn-y <tanyujun16@gmail.com>
2026-08-27 13:35:20 +0800

    Change bot name

---
+++ src/main/java/Clammy.java
@@ -0,0 +1,8 @@
+public class Clammy {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        String startMessage = "Hello! I'm Clammy.\nWhat can I do for you?\n";
+        String endMessage = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + startMessage + line +endMessage + line);
+    }
+}
