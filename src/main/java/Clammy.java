85cc2c72b79d364ffd10c99de088d0af752d3b88
J-unn-y <tanyujun16@gmail.com>
2026-08-27 13:53:50 +0800

    Add Echo functionality

--- src/main/java/Clammy.java
+++ src/main/java/Clammy.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Clammy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________\n";
         String startMessage = "Hello! I'm Clammy.\nWhat can I do for you?\n";
         String endMessage = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + startMessage + line +endMessage + line);
+
+        System.out.println(line + startMessage + line);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(line + endMessage + line);
+                break;
+            }
+            System.out.println(line + command + "\n" + line);
+        }
     }
 }
